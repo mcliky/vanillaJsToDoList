c685d3d4df3a516d574d6dab98464f9516f9a8bf
mcliky <tmaszuraev@gmail.com>
2023-08-17 12:42:33 -0400

    made it look better

--- app.js
+++ app.js
@@ -5,43 +5,59 @@ let number = 1;
 let itemName = "Item 2";
 let status = "Todo";
 let todoList = document.querySelector(".todo-list");
+let parent;
+let prevParent;
+
 addBtn.addEventListener("click", () => {
   let todoItem = `<li class="todo-item">
 <span>${number}</span> <span>${todoInput.value} </span><span>${status}</span>
-<button class="edit-button">Edit</button>
-<button class="remove-button">Remove</button>
+<button data-action="edit">Edit</button>
+<button data-action="remove">Remove</button>
 </li> `;
   number += 1;
   todoList.innerHTML += todoItem;
   todoInput.value = "";
 });
 
-todoList.addEventListener("click", (event) => {
-  if (event.target.classList.contains("remove-button")) {
-    event.target.parentElement.remove();
-  } else if (event.target.classList.contains("edit-button")) {
-    const parent = event.target.parentElement;
-    const prevParent = parent.innerHTML;
-
-    parent.innerHTML = `<div class="edit-value">
-      <input class="edit-text" type="text" placeholder="new name" />
-      <button class="edit-todo" type="button">Edit name</button>
-      <button class="cancel-edit" type="button">Cancel</button>
-    </div>`;
-
-    const editValue = parent.querySelector(".edit-value");
-
-    editValue.addEventListener("click", (event) => {
-      if (event.target.classList.contains("edit-todo")) {
-        parent.innerHTML = `<span>${number}</span> <span>${
-          parent.querySelector(".edit-text").value
-        } </span><span>${status}</span>
-       <button class="edit-button">Edit</button>
-       <button class="remove-button">Remove</button>
-      `;
-      } else if (event.target.classList.contains("cancel-edit")) {
-        parent.innerHTML = prevParent;
-      }
-    });
+class Element {
+  constructor(elem) {
+    elem.onclick = this.onClick.bind(this);
   }
-});
+
+  edit(event) {
+    parent = event.target.parentElement;
+    prevParent = parent.innerHTML;
+    parent.innerHTML = `<div class="edit-value">
+ <input class="edit-text" type="text" placeholder="new name" />
+ <button data-action="editTodo" type="button">Edit name</button>
+ <button data-action="cancelEdit" type="button">Cancel</button>
+</div>`;
+  }
+
+  editTodo(event) {
+    number -= 1;
+    parent.innerHTML = `<span>${number}</span> <span>${
+      parent.querySelector(".edit-text").value
+    } </span><span>${status}</span>
+   <button class="edit-button">Edit</button>
+   <button class="remove-button">Remove</button>
+  `;
+  }
+
+  cancelEdit(event) {
+    parent.innerHTML = prevParent;
+  }
+
+  remove(event) {
+    event.target.parentElement.remove();
+    number -= 1;
+  }
+
+  onClick(event) {
+    let action = event.target.dataset.action;
+    if (action) {
+      this[action](event);
+    }
+  }
+}
+new Element(todoList);
