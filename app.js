424db217356c8c0665d7cac175a020be90023f4a
mcliky <tmaszuraev@gmail.com>
2023-08-23 18:56:17 -0400

    change rmvBtn

--- app.js
+++ app.js
@@ -1,89 +1,112 @@
-const todoInput = document.querySelector(".todo-text");
-const addBtn = document.querySelector(".add-todo");
-const remvBtn = document.querySelector("remove-button");
-let number = 1;
-let itemName = "Item 2";
-let status = "Todo";
-let todoList = document.querySelector(".todo-list");
-let checkEdtFlag = false;
+const todoInput = document.querySelector('.todo-text')
+const addBtn = document.querySelector('.add-todo')
+const remvBtn = document.querySelector('remove-button')
+let number = 1
+let itemName = 'Item 2'
+let status = 'Todo'
+let todoList = document.querySelector('.todo-list')
+let checkEdtFlag = false
 
-addBtn.addEventListener("click", (event) => {
-  if (todoInput.value === "") {
-    event.target.parentElement.querySelector(".warning").style.display =
-      "inline-block";
+addBtn.addEventListener('click', (event) => {
+  if (todoInput.value === '') {
+    event.target.parentElement.querySelector('.warning').style.display =
+      'inline-block'
   } else {
-    event.target.parentElement.querySelector(".warning").style.display = "none";
+    event.target.parentElement.querySelector('.warning').style.display = 'none'
     let todoItem = `<li class="todo-item">
-<span>${number}</span> <span>${todoInput.value} </span><span>${status}</span>
+<span class="item${number}">${number}</span> <span class="item-name">${todoInput.value} </span><span>${status}</span>
 <button data-action="edit">Edit</button>
-<button data-action="remove">Remove</button>
-</li> `;
-    number += 1;
-    todoList.innerHTML += todoItem;
-    todoInput.value = "";
+<button data-action="removeBtn">Remove</button>
+</li> `
+    number += 1
+    todoList.innerHTML += todoItem
+    todoInput.value = ''
   }
-});
+})
 
 class Element {
   constructor(elem) {
-    this.elem = elem;
-    this.parent;
-    this.prevParent;
+    this.elem = elem
+    this.parent
+    this.prevParent
 
-    elem.addEventListener("click", this.onClick.bind(this));
+    elem.addEventListener('click', this.onClick.bind(this))
   }
 
   edit(event) {
     if (checkEdtFlag === true) {
-      return;
+      return
     } else {
-      checkEdtFlag = true;
-      this.parent = event.target.parentElement;
-      this.prevParent = this.parent.innerHTML;
+      checkEdtFlag = true
+      this.parent = event.target.parentElement
+      this.prevParent = this.parent.innerHTML
+      let itemName = this.parent.querySelector('.item-name').innerHTML
       this.parent.innerHTML = `<div class="edit-value">
- <input class="edit-text" type="text" placeholder="new name" />
+ <input class="edit-text" value=${itemName} type="text" placeholder="new name" />
  <button data-action="editTodo" type="button">Edit name</button>
  <button data-action="cancelEdit" type="button">Cancel</button>
  <p class='warning'>No input</p>
-</div>`;
+</div>`
     }
   }
 
   editTodo(event) {
-    console.log(this.parent);
-    this.parent = event.target.parentElement;
-    const editText = this.parent.querySelector(".edit-text");
-    console.log(this.parent);
-    if (editText.value === "") {
-      this.parent.querySelector(".warning").style.display = "block";
+    this.parent = event.target.parentElement
+    const editText = this.parent.querySelector('.edit-text')
+    if (editText.value === '') {
+      this.parent.querySelector('.warning').style.display = 'block'
     } else {
-      this.parent.querySelector(".warning").style.display = "none";
-      this.parent.parentElement.innerHTML = `<span>${number}</span> <span>${editText.value} </span><span>${status}</span>
-   <button data-action="edit">Edit</button> 
-   <button data-action="remove">Remove</button>
-  `;
-      checkEdtFlag = false;
+      this.parent.querySelector('.warning').style.display = 'none'
+      this.parent = this.parent.parentElement
+      console.log(this.prevParent)
+      this.parent.innerHTML = this.prevParent
+      this.parent.querySelector('.item-name').innerHTML = editText.value
+      checkEdtFlag = false
     }
   }
 
   cancelEdit(event) {
-    console.log(event);
-    this.parent.innerHTML = this.prevParent;
-    checkEdtFlag = false;
+    console.log(event)
+    this.parent.innerHTML = this.prevParent
+    checkEdtFlag = false
   }
 
-  remove(event) {
-    this.parent = event.target.parentElement;
-    this.parent.remove();
-    number -= 1;
+  removeBtn(event) {
+    let grandParent = event.target.parentElement.parentElement
+    this.parent = event.target.parentElement
+    let length = number
+    if (this.parent.querySelector(`.item${number - 1}`)) {
+      this.parent.remove()
+      number -= 1
+    } else {
+      let i = 1
+      while (i < length) {
+        console.log(i)
+        if (this.parent.querySelector(`.item${i}`)) {
+          if (this.parent.querySelector(`.item${i}`).innerHTML !== 1) {
+            number -= 1
+          }
+          this.parent.remove()
+        }
+        if (Number(grandParent.querySelector(`.item${i}`).innerHTML) === 1) {
+          i += 1
+          continue
+        } else {
+          grandParent.querySelector(`.item${i}`).innerHTML = (
+            Number(grandParent.querySelector(`.item${i}`).innerHTML) - 1
+          ).toString()
+        }
+        i += 1
+      }
+    }
   }
 
   onClick(event) {
-    let action = event.target.dataset.action;
+    let action = event.target.dataset.action
     if (action) {
-      this[action](event);
+      this[action](event)
     }
   }
 }
 
-new Element(todoList);
+new Element(todoList)
